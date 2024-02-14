77078bb638d2442ea7856d62a7c659d57c299779
Cordeiro Silva Chaves <mocv@gft.com>
2024-02-14 13:25:15 -0300

    Teste movimentação futura

--- src/main/java/marco/chaves/tests/MovimentacaoTest.java
+++ src/main/java/marco/chaves/tests/MovimentacaoTest.java
@@ -3,11 +3,15 @@ package marco.chaves.tests;
 import marco.chaves.core.BaseTest;
 import marco.chaves.page.MenuPage;
 import marco.chaves.page.MovimentacaoPage;
+import marco.chaves.utils.DataUtils;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
+
+import static marco.chaves.utils.DataUtils.obterDataFormatada;
 
 public class MovimentacaoTest extends BaseTest {
     private MenuPage menuPage = new MenuPage();
@@ -39,5 +43,25 @@ public class MovimentacaoTest extends BaseTest {
                 "Descrição é obrigatório", "Interessado é obrigatório",
                 "Valor é obrigatório", "Valor deve ser um número")));
         Assert.assertEquals(6, erros.size());
+    }
+
+    @Test
+    public void testInserirMovimentacaoFutura(){
+        menuPage.acessarTelaInserirMovimentacao();
+
+        Date dataFutura = DataUtils.obterDataComDiferencaDias(5);
+
+        movPage.setDataMovimentacao(obterDataFormatada(dataFutura));
+        movPage.setDataPagamento(obterDataFormatada(dataFutura));
+        movPage.setDescricao("Movimentação do Teste");
+        movPage.setInteressado("Interessado");
+        movPage.setValor("500");
+        movPage.setConta("Conta do Teste alterada");
+        movPage.setStatusPago();
+        movPage.salvar();
+
+        List<String>erros  = movPage.obterErros();
+        Assert.assertTrue(erros.contains("Data da Movimentação deve ser menor ou igual à data atual"));
+        Assert.assertEquals(1, erros.size());
     }
 }
