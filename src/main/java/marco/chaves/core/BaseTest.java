64841d3b7f3d9fde7d1cf696e6840995f6803836
Cordeiro Silva Chaves <mocv@gft.com>
2024-02-20 13:56:36 -0300

    Execução em parallismo e Thread

--- src/main/java/marco/chaves/core/BaseTest.java
+++ src/main/java/marco/chaves/core/BaseTest.java
@@ -1,7 +1,9 @@
 package marco.chaves.core;
 
+import marco.chaves.page.LoginPage;
 import org.apache.commons.io.FileUtils;
 import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.rules.TestName;
 import org.openqa.selenium.OutputType;
@@ -14,10 +16,19 @@ import static marco.chaves.core.DriverFactory.getDriver;
 import static marco.chaves.core.DriverFactory.killDriver;
 
 public class BaseTest {
-
-
     @Rule
     public TestName testName = new TestName();
+
+    private static LoginPage page = new LoginPage();
+
+    @Before
+    public void inicializa(){
+        page.acessarTelaInicial();
+
+        page.setEmail("macschaves@gmail.com");
+        page.setSenha("250978");
+        page.entrar();
+    }
 
     @After
     public void finaliza() throws IOException {
--- src/main/java/marco/chaves/core/DriverFactory.java
+++ src/main/java/marco/chaves/core/DriverFactory.java
@@ -7,12 +7,22 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class DriverFactory {
 
-    private static WebDriver driver;
+    //private static WebDriver driver;
+    private static ThreadLocal<WebDriver> threadDriver = new ThreadLocal<WebDriver>(){
+        @Override
+        protected synchronized WebDriver initialValue(){
+            return initDriver();
+        }
+    };
 
     private DriverFactory(){
     }
-
     public static WebDriver getDriver(){
+        return threadDriver.get();
+    }
+
+    public static WebDriver initDriver(){
+        WebDriver driver = null;
         if (driver ==null){
             switch (Propriedades.browser){
                 case FIREFOX: driver = new FirefoxDriver(); break;
@@ -24,9 +34,13 @@ public class DriverFactory {
     }
 
     public static void killDriver(){
+        WebDriver driver = getDriver();
         if (driver != null) {
             driver.quit();
             driver = null;
+        }
+        if (threadDriver != null){
+            threadDriver.remove();
         }
     }
 }
