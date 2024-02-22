11a74139c953d6acd86f3d0dc014cb8c888ee9a0
Cordeiro Silva Chaves <mocv@gft.com>
2024-02-22 10:53:01 -0300

    GRID

--- src/main/java/marco/chaves/core/DriverFactory.java
+++ src/main/java/marco/chaves/core/DriverFactory.java
@@ -4,6 +4,11 @@ import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class DriverFactory {
 
@@ -23,13 +28,26 @@ public class DriverFactory {
 
     public static WebDriver initDriver(){
         WebDriver driver = null;
-        if (driver ==null){
-            switch (Propriedades.browser){
+        if(Propriedades.TIPO_EXECUCAO == Propriedades.TipoExecucao.LOCAL) {
+            switch (Propriedades.BROWSER) {
                 case FIREFOX: driver = new FirefoxDriver(); break;
                 case CHROME: driver = new ChromeDriver(); break;
             }
-            driver.manage().window().setSize(new Dimension(1200, 765));
         }
+        if(Propriedades.TIPO_EXECUCAO == Propriedades.TipoExecucao.GRID) {
+            DesiredCapabilities cap = null;
+            switch (Propriedades.BROWSER) {
+                case FIREFOX: cap = DesiredCapabilities.firefox(); break;
+                case CHROME: cap = DesiredCapabilities.chrome(); break;
+            }
+            try {
+                driver = new RemoteWebDriver(new URL("http://192.168.0.184:4444/wd/hub"), cap);
+            } catch (MalformedURLException e) {
+                System.err.println("Falha na conexão com o GRID");
+                e.printStackTrace();
+            }
+        }
+        driver.manage().window().setSize(new Dimension(1200, 765));
         return driver;
     }
 
--- src/main/java/marco/chaves/core/Propriedades.java
+++ src/main/java/marco/chaves/core/Propriedades.java
@@ -2,14 +2,21 @@ package marco.chaves.core;
 
 public class Propriedades {
 
-    public static boolean FECHAR_BROWSER = false;
+    public static boolean FECHAR_BROWSER = true;
     // true - abre e fecha o browser a cada execução, almentando o tempo de teste.
     // false - abre o browser apenas uma vez, otimizando o tempo de execução
 
-    public static Browsers browser = Browsers.CHROME;
+    public static Browsers BROWSER = Browsers.FIREFOX;
+
+    public static TipoExecucao TIPO_EXECUCAO = TipoExecucao.GRID;
 
     public enum Browsers {
         CHROME,
         FIREFOX
+    }
+
+    public enum TipoExecucao {
+        LOCAL,
+        GRID
     }
 }
